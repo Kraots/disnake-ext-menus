dd3c9704464780214beec422d539e349c00cb591
Kraots <nagit.robermessi@gmail.com>
2021-11-25 16:49:29 +0200

    Added long_description

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
+import pathlib
 from setuptools import setup
 
-setup(name='disnake-ext-menus',
-      author='Rapptz, Kraots',
-      url='https://github.com/Kraots/disnake-ext-menus',
-      version='0.0.1',
-      packages=['disnake.ext.menus'],
-      description='An extension module to make reaction based menus with disnake',
-      install_requires=['disnake'],
-      python_requires='>=3.8.0'
+ROOT = pathlib.Path(__file__).parent
+
+with open(ROOT / 'README.md', 'r', encoding='utf-8') as f:
+    README = f.read()
+
+setup(
+    name='disnake-ext-menus',
+    author='Rapptz, Kraots',
+    url='https://github.com/Kraots/disnake-ext-menus',
+    version='0.0.1',
+    packages=['disnake.ext.menus'],
+    description='An extension module to make reaction based menus with disnake',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    install_requires=['disnake'],
+    python_requires='>=3.8.0'
 )
