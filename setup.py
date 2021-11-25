816fbd74ecd9f5df9a15efcde4f01c6d96e02124
Kraots <nagit.robermessi@gmail.com>
2021-11-25 16:41:58 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='disnake-ext-menus',
+      author='Rapptz, Kraots',
+      url='https://github.com/Kraots/disnake-ext-menus',
+      version='0.0.1',
+      packages=['disnake.ext.menus'],
+      description='An extension module to make reaction based menus with disnake',
+      install_requires=['disnake'],
+      python_requires='>=3.8.0'
+)
